b441e3143b16fe08adca17bab75cba50b5e1f7a5
David Rivera A <davidriveraarbelaez@gmail.com>
2024-07-31 19:37:07 -0500

    SystemInfo

--- Unidad_5/01_install_libs_packages_NPM.js
+++ Unidad_5/01_install_libs_packages_NPM.js
@@ -1,5 +1,4 @@
 /*
-
-*/
-npm list; // muestra las dependencias de un proyecto
 npm list --depth=0; // muestra las dependencias de un proyecto sin profundidad
+npm list; // muestra las dependencias de un proyecto
+*/
--- Unidad_5/system_info.js
+++ Unidad_5/system_info.js
@@ -22,18 +22,23 @@ const systemInfo = {
     osType: os.type(),                // Obtiene el nombre del sistema operativo
     architecture: os.arch(),          // Obtiene la arquitectura del sistema
     cpu: os.cpus()[0].model,          // Modelo de la CPU
-    /*
     freeMemory: os.freemem(),         // Memoria libre del sistema
+    /////////
     totalMemory: os.totalmem(),       // Memoria total del sistema
     rootDir: os.homedir(),            // Directorio raíz
-    userDir: os.userInfo().Homedir,   // Directorio de usario
-    */
-// Nombre del host
-// Obtener la versión de la plataforma del sistema
-// Obtener la versión de Node.js
+    userDir: os.userInfo().homedir,   // Directorio de usario
+    hostName: os.hostname(),          // Nombre del host
+    platform: os.platform(),          // Obtener la versión de la plataforma del sistema
+    nodeVersion: process.version      // Obtener la versión de Node.js
 };
 
 console.log("Información del sistema operativo");
 console.log(`Nombre del sistema operativo: ${systemInfo.osType}`);
 console.log(`Arquitectura: ${systemInfo.architecture}`);
 console.log(`CPU: ${systemInfo.cpu}`);
+console.log(`Memoria Libre: ${systemInfo.freeMemory}`);
+console.log(`Memoria Total: ${systemInfo.totalMemory}`);
+console.log(`Directorio raíz: ${systemInfo.rootDir}`);
+console.log(`Nombre del host: ${systemInfo.hostName}`);
+console.log(`Plataforma del sistema: ${systemInfo.platform}`);
+console.log(`Versión de Nodejs: ${systemInfo.nodeVersion}`);
