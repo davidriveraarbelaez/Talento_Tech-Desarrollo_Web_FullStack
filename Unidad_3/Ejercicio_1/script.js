090a40f7c1330fb13fc2924d400a099375440fc4
David Rivera A <davidriveraarbelaez@gmail.com>
2024-08-05 09:05:11 -0500

    Create script.js

---
+++ Unidad_3/Ejercicio_1/script.js
@@ -0,0 +1,13 @@
+/* 
+Adivina el número 
+*/
+
+// Generar un número aleatorio
+let NumeroAleatorio = Math.random();
+// Si son iguales imprimir "Has ganado"
+if(NumeroAleatorio === numeroIngresado){
+
+}else{
+    // Si son diferentes imprimir "Sigue participando"
+
+}
