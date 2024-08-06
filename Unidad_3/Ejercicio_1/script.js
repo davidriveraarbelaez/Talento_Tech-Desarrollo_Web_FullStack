bd94219a2480ecf193badb03e9c51909df150bb9
David Rivera A <davidriveraarbelaez@gmail.com>
2024-08-06 07:06:34 -0500

    Update script.js

--- Unidad_3/Ejercicio_1/script.js
+++ Unidad_3/Ejercicio_1/script.js
@@ -1,13 +1,22 @@
 /* 
-Adivina el número 
+Adivina el número entre 1 y 10
 */
 
 // Generar un número aleatorio
-let NumeroAleatorio = Math.random();
-// Si son iguales imprimir "Has ganado"
-if(NumeroAleatorio === numeroIngresado){
+let numeroAleatorio = 10*Math.random();
+let NumeroAleatorio = Math.ceil(numeroAleatorio);
 
+// Ingresar número
+// let numeroIngresado = 8;
+let numeroIngresado = prompt("Ingresa un número del 1 al 10: ");
+console.log("aleatorio: " + NumeroAleatorio);
+console.log("ingresado: " + numeroIngresado);
+
+// Si son iguales imprimir "Has ganado"
+if(NumeroAleatorio == numeroIngresado){
+    console.log("Has ganado");
 }else{
     // Si son diferentes imprimir "Sigue participando"
-
+    // console.log("Sigue participando");
+    alert("Sigue participando, el número ingresado fue " + numeroIngresado + " y el ganador era " + NumeroAleatorio);
 }
