760d731b314938173144d3f8d9e10ad86fd3c7e0
davidriveraarbelaez <davidriveraarbelaez@gmail.com>
2024-08-20 12:05:15 +0000

    taller js

---
+++ Unidad_3/Taller/basico.js
@@ -0,0 +1,13 @@
+/* Nivel Básico:
+Tipos de Datos y Variables:
+1. Crear un programa que solicite al usuario su nombre y edad, y luego muestre un mensaje de bienvenida personalizado.
+2. Declarar variables para almacenar diferentes tipos de datos (número, cadena, booleano) y realizar operaciones básicas.
+
+Operadores Aritméticos y Comparación:
+3. Calcular el área y perímetro de un rectángulo, solicitando al usuario las medidas de sus lados.
+4. Implementar un programa que determine si un número es par o impar.
+
+Condicionales:
+5. Crear un programa que simule una calculadora básica, permitiendo al usuario elegir la operación a realizar.
+6. Desarrollar un programa que determine si un año es bisiesto.
+*/
