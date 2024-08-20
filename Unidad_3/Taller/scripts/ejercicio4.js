693bb0868cd858914667fd94dbd45391cf3fe11b
davidriveraarbelaez <davidriveraarbelaez@gmail.com>
2024-08-20 12:56:49 +0000

    estilos

---
+++ Unidad_3/Taller/scripts/ejercicio4.js
@@ -0,0 +1,9 @@
+// Implementar un programa que determine si un número es par o impar.
+
+let numero = 101; // Modificar con prompt si se quiere trabajar de forma dinámica
+
+if (numero % 2  === 0){
+    console.log("Par");
+}else{
+    console.log("Impar");
+}
