84c44ac64274bcc9891b9981b94af89b67b0ff2d
David Rivera A <davidriveraarbelaez@gmail.com>
2024-09-06 08:13:51 -0500

    Frontend JSON

--- Unidad_6/integracionFullStack/ejercicio_1/server.js
+++ Unidad_6/integracionFullStack/ejercicio_1/server.js
@@ -1,8 +1,7 @@
-// Importar express, body-parser, fs y cors
+// Importar las dependencias Express, Cors y BodyParser
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const app = express(); // Crear una instancia de Express
 const port = 3000; // Asignar el puerto para la aplicación
 
@@ -10,17 +9,13 @@ app.use(cors()); // Habilitar Cors
 app.use(bodyParser.json()); // Habilitar BodyParser
 app.use(express.static('public')); // Habilitar la carpeta public
 
-// Ruta para obtener datos desde un archivo JSON
-app.get('/api/data', (req,res) => {
-    fs.readFile('data.json', 'utf-8',(err, data) => {  
-        if (err){
-            console.error('Error al leer el archivo JSON', 'err');
-            res.status(500).json({ message: 'Error al leer los datos'});
-            return;
-        };
-        res.json(JSON.parse(data));
-      });
-})
+// Ruta de ejemplo para recibir datos desde el frontend
+app.post('/api/data', (req,res) =>{    // Ruta post para recibir datos
+    const data = req.body; // Obtener datos del body
+    console.log('Datos recibidos: ', data); // Imprimir los datos en consola
+
+    res.json({ message: 'Datos recibidos correctamente', receivedData: data });// Enviar una respuesta al frontend
+})   
 
 // Iniciar el servidor en el puerto asignado
 app.listen(port, () => {
--- Unidad_6/integracionFullStack/public/script.js
+++ Unidad_6/integracionFullStack/public/script.js
@@ -1,17 +1,29 @@
-document.getElementById('dataForm').addEventListener('submit', async function(event) {
-    event.preventDefault(); // Evitar que se recargue la página
-    const name = document.getElementById('name').value; // Obtiene el valor del input con el id 'name'
+window.onload = async function () {
+    try{
+        const response = await fetch('/api/data');
+        const data = await response.json();
 
-    /*
-    fetch sirve para enviar peticiones HTTP a un servidor y recibir respuestas.
-    */
-    const response = await fetch('/api/data',{ // Enviar una petición POST a la ruta 'api/data' con los datos del formulario
-        method: 'POST', // Indicar el método de la petición, en nuestro caso POST
-        headers: { // Indicar el contenido del cuerpo de la petición, en nuestro caso un objeto JSON con el nombre
-            'Content-Type': 'application/json' // indicar que el contenido es de tipo JSON
-        },
-        body: JSON.stringify({ name })// Convertir el objeto a un string JSON y enviarlo en el cuerpo de la petición
-    }); // Esperar la respuesta del servidor
-    const result = await response.json(); // Convertir la respuesta a un objeto JSON
-    document.getElementById('responseMessage').textContent = result.message; // Mostrar el mensaje de respuesta en el elemento con el id 'responseMessage'
-}) // Fin de la función que se ejecuta al enviar el formulario
+        const tbody = document.querySelector('#userTable tbody');
+        data.forEach(user => {
+            const tr = document.createElement('tr');
+
+            const tdId = document.createElement('td');
+            tdId.textContent = user.id;
+            tr.appendChild(tdId);
+ 
+/*
+            const tdName = document.createElement('td');
+            tdId.textContent = user.name;
+            tr.appendChild(tdName);
+
+            const tdEmail = document.createElement('td');
+            tdId.textContent = user.email;
+            tr.appendChild(tdEmail);
+*/
+            tbody.appendChild(tr);
+        });
+
+    }catch(error){
+        console.error('Error al cargar los datos', error)
+    }    
+}
